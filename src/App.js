30ac182baecd6a2e05bb8c758d60c718862e8b90
Dal-Ress <zhuravsky.vlad@gmail.com>
2020-10-16 12:46:56 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, NavLink, Route} from "react-router-dom";
+
+const Menu = ()=>{
+  return <ul>
+    <li><NavLink className="nav-link" to="/profile">Профиль</NavLink></li>
+    <li><NavLink className="nav-link" to="/settings">Настройки</NavLink></li>
+    <li><NavLink className="nav-link" to="/users">Пользователи</NavLink></li>
+  </ul>
+}
+
+const Profile = ()=>{
+    return <div className="row">
+        <div className="col-2">
+            <img width="100%" src="https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png" alt=""/>
+        </div>
+        <div className="col-10">
+            <h1>Иван Иванов</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis dolorum quis voluptate. Amet asperiores facilis natus repudiandae sed unde voluptatem. Ad commodi dolor ea eius, labore porro ratione similique sit?</p>
+        </div>
+    </div>
+}
+
+const Settings = ()=>{
+    return <div>
+        <p>Имя: Иван</p>
+        <p>Фамилия: Иванов</p>
+        <p>E-mail: ivanov@gmail.com</p>
+    </div>
+}
+
+const Users = ()=>{
+    return <table className="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Имя</th>
+            <th scope="col">Фамилия</th>
+            <th scope="col">Username</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+        </tr>
+        </tbody>
+    </table>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="container-fluid">
+       <BrowserRouter>
+         <div className="row">
+           <div className="col-3">
+             <Menu/>
+           </div>
+           <div className="col-9">
+               <Route path="/profile" render={()=>{return <Profile/>}}/>
+               <Route path="/settings" render={()=>{return <Settings/>}}/>
+               <Route path="/users" render={()=>{return <Users/>}}/>
+           </div>
+         </div>
+       </BrowserRouter>
+   </div>
   );
 }
 
